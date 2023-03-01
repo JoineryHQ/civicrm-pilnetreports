ed46b1c9ee128beb5aee13d4e8a3f02fe4c0e2ab
Allen Shaw <allen@JoineryHQ.com>
2023-03-01 16:15:15 -0600

    Initial commit

---
+++ pilnetreports.php
@@ -0,0 +1,108 @@
+<?php
+
+require_once 'pilnetreports.civix.php';
+// phpcs:disable
+use CRM_Pilnetreports_ExtensionUtil as E;
+// phpcs:enable
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
+ */
+function pilnetreports_civicrm_config(&$config) {
+  _pilnetreports_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
+ */
+function pilnetreports_civicrm_install(): void {
+  _pilnetreports_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function pilnetreports_civicrm_postInstall(): void {
+  _pilnetreports_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
+ */
+function pilnetreports_civicrm_uninstall(): void {
+  _pilnetreports_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
+ */
+function pilnetreports_civicrm_enable(): void {
+  _pilnetreports_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
+ */
+function pilnetreports_civicrm_disable(): void {
+  _pilnetreports_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
+ */
+function pilnetreports_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _pilnetreports_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function pilnetreports_civicrm_entityTypes(&$entityTypes): void {
+  _pilnetreports_civix_civicrm_entityTypes($entityTypes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function pilnetreports_civicrm_preProcess($formName, &$form): void {
+//
+//}
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
+ */
+//function pilnetreports_civicrm_navigationMenu(&$menu): void {
+//  _pilnetreports_civix_insert_navigation_menu($menu, 'Mailings', [
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ]);
+//  _pilnetreports_civix_navigationMenu($menu);
+//}
